4360983acc16d57293591cd5ff18130d08329294
Devin Leonhart <devinleonhart@fastmail.com>
2021-03-11 11:42:09 -0800

    Finish universe spec.

--- spec/features/universe_spec.rb
+++ spec/features/universe_spec.rb
@@ -2,24 +2,34 @@ require "rails_helper"
 
 RSpec.feature "Universe", :type => :feature do
 
-  scenario "should allow the creation of a universe with correct fields." do
+  background {
     login_as(FactoryBot.create(:user, {email: "test@test.com", password: "abc123"}))
     visit new_universe_url
+  }
 
-    fill_in 'Name', with: "A Brand New Universe"
-    find_button('Save').click
+  scenario "should allow the creation of a universe with correct fields." do
+    fill_in "Name", with: "A Brand New Universe"
+    find_button("Save").click
+
+    expect(page).to have_text("Universe created!")
+    expect(page).to have_text("A Brand New Universe")
+    expect(Universe.count).to eq(1)
+    expect(Universe.all.first.name).to eq("A Brand New Universe")
+  end
+
+  scenario "should allow the creation of a universe with correct fields." do
+    fill_in "Name", with: "A Brand New Universe"
+    find_button("Save").click
 
     expect(page).to have_text("Universe created!")
     expect(page).to have_text("A Brand New Universe")
   end
 
   scenario "should not allow the creation of a universe with a missing name." do
-    login_as(FactoryBot.create(:user, {email: "test@test.com", password: "abc123"}))
-    visit new_universe_url
-
-    fill_in 'Name', with: ""
-    find_button('Save').click
+    fill_in "Name", with: ""
+    find_button("Save").click
 
     expect(page).to have_text("Name can't be blank")
+    expect(Universe.count).to eq(0)
   end
 end
