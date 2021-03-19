df5dc1ce178ae202805dd8082987af81dff7f31f
Devin Leonhart <devinleonhart@fastmail.com>
2021-03-19 10:14:16 -0700

    Update session tests.

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -8,16 +8,36 @@ RSpec.feature "Session", type: :feature do
     visit new_user_session_url
   end
 
-  scenario "should allow the user to log in with correct credentials." do
+  scenario "should allow the user to sign in and out." do
     fill_in("Email", with: "user@test.com")
     fill_in("Password", with: "abc123")
     find_button("Sign In").click
 
     expect(page).to have_text("Signed in successfully.")
+
+    find_link("Sign out").click
+
+    expect(page).to have_text("You need to sign in or sign up before continuing.")
+  end
+
+  scenario "should not allow a user to log in with no email provided." do
+    fill_in("Email", with: "")
+    fill_in("Password", with: "abc123")
+    find_button("Sign In").click
+
+    expect(page).to have_text("Invalid Email or password.")
   end
 
   scenario "should not allow a user to log in with an email that doesn't exist." do
-    fill_in("Email", with: "wrong email")
+    fill_in("Email", with: "wrongemail@test.com")
+    fill_in("Password", with: "abc123")
+    find_button("Sign In").click
+
+    expect(page).to have_text("Invalid Email or password.")
+  end
+
+  scenario "should not allow a user to log in with no password provided." do
+    fill_in("Email", with: "")
     fill_in("Password", with: "abc123")
     find_button("Sign In").click
 
@@ -31,4 +51,5 @@ RSpec.feature "Session", type: :feature do
 
     expect(page).to have_text("Invalid Email or password.")
   end
+
 end
