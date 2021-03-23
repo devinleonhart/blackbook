7e5e0fea14e81c52cfe32cb7616cacd70cde8de4
Devin Leonhart <devinleonhart@fastmail.com>
2021-03-23 16:44:29 -0700

    Add tests.

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -25,4 +25,27 @@
 require "rails_helper"
 
 RSpec.describe Character, type: :model do
+  before do
+    @universe = FactoryBot.create(:universe)
+    @character = FactoryBot.build(:character, { universe: @universe })
+    @other_character = FactoryBot.build(:character, { universe: @universe })
+    @mutual_relationship = FactoryBot.create(:mutual_relationship, { character1: @character, character2: @other_character })
+  end
+
+  it "should not allow a character to have a missing name" do
+    @character.name = ""
+    expect(@character).to be_invalid
+  end
+
+  it "should not allow a duplicate character name" do
+    @character.name = "Max Lionheart"
+    @character.save
+    @other_character.name = "Max Lionheart"
+    expect(@other_character).to be_invalid
+  end
+
+  it "should list its relationships" do
+    expect(@mutual_relationship.relationships).to include(@character.relationships.first)
+    expect(@mutual_relationship.relationships).to include(@character.relationships.last)
+  end
 end
