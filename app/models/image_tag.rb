31e38c55e24197a26039ffb4845535c4c09c3aa5
Devin Leonhart <devinleonhart@fastmail.com>
2021-03-23 17:36:36 -0700

    Add tests.

--- app/models/image_tag.rb
+++ app/models/image_tag.rb
@@ -20,8 +20,20 @@
 class ImageTag < ApplicationRecord
   validates :character, uniqueness: { scope: :image_id }
 
+  validate :character_must_be_from_same_universe_as_image
+
   belongs_to :character, inverse_of: :image_tags
   belongs_to :image, inverse_of: :image_tags
 
   delegate :universe, to: :character, allow_nil: true
+
+  private
+
+  def character_must_be_from_same_universe_as_image
+    return if character.nil? || image.nil?
+
+    if character.universe != image.universe
+      errors.add(:base, "The character and image must be from the same universe!")
+    end
+  end
 end
--- spec/models/image_tag_spec.rb
+++ spec/models/image_tag_spec.rb
@@ -20,4 +20,39 @@
 require "rails_helper"
 
 RSpec.describe ImageTag, type: :model do
+  before do
+    @universe = FactoryBot.create(:universe)
+    @other_universe = FactoryBot.create(:universe)
+    @character = FactoryBot.create(:character, { universe: @universe })
+    @image1 = FactoryBot.create(:image, { universe: @universe })
+    @image2 = FactoryBot.create(:image, { universe: @other_universe })
+  end
+
+  it "should create a valid image_tag" do
+    @image_tag = build(:image_tag, character: @character, image: @image1)
+    expect(@image_tag).to be_valid
+  end
+
+  it "should create be invalid when character is missing" do
+    @image_tag = build(:image_tag, character: nil, image: @image1)
+    expect(@image_tag).to be_invalid
+  end
+
+  it "should create be invalid when image is missing" do
+    @image_tag = build(:image_tag, character: @character, image: nil)
+    expect(@image_tag).to be_invalid
+  end
+
+  it "should not allow a duplicate image tag" do
+    @image_tag1 = create(:image_tag, character: @character, image: @image1)
+    @image_tag2 = build(:image_tag, character: @character, image: @image1)
+    expect(@image_tag1).to be_valid
+    expect(@image_tag2).to be_invalid
+  end
+
+  it "should not allow a the tagging of a character from a different universe" do
+    @image_tag1 = build(:image_tag, character: @character, image: @image2)
+    expect(@image_tag1).to be_invalid
+    expect(@image_tag1.errors.full_messages).to eq(["The character and image must be from the same universe!"])
+  end
 end
