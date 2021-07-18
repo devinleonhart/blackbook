960844342bd5e3ccaeaf18b3cb44a5f9d45561de
Devin Leonhart <devinleonhart@fastmail.com>
2021-07-18 00:29:48 -0700

    Add proxy tags.

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -16,9 +16,9 @@ module ImageHelper
 
   def generate_image_tag(image, size)
     if image.image_file.filename.extension == "gif"
-      image_tag(image.image_file, class: "img-thumbnail")
+      image_tag(rails_storage_proxy_path(image.image_file), class: "img-thumbnail")
     else
-      image_tag(image.image_file.variant(resize_to_limit: size), class: "img-thumbnail")
+      image_tag(rails_storage_proxy_path(image.image_file.variant(resize_to_limit: size)), class: "img-thumbnail")
     end
   end
 end
