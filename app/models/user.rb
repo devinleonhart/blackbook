d40d435b3929438737cc254cd4186d2525cc51ff
Devin Leonhart <devinleonhart@fastmail.com>
2022-02-18 15:23:34 -0800

    Finish migration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,3 @@
-# frozen_string_literal: true
-
-# == Schema Information
-#
-# Table name: users
-#
-#  id                     :bigint           not null, primary key
-#  display_name           :citext           not null
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  remember_created_at    :datetime
-#  reset_password_sent_at :datetime
-#  reset_password_token   :string
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#
-# Indexes
-#
-#  index_users_on_display_name          (display_name) UNIQUE
-#  index_users_on_email                 (email) UNIQUE
-#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
-#
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
