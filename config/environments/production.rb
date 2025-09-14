954c6a6c6e26ac55be8573570b3c83a7a1166446
Devin Leonhart <devinleonhart@fastmail.com>
2025-09-13 20:33:22 -0700

    Enable hybrid mode.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Digital Ocean stores prod assets
-  config.active_storage.service = :digitalocean
+  config.active_storage.service = :hybrid
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
