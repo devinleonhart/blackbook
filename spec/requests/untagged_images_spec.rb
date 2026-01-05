443074d8855250e339206ca8f4cc5bd94fb063d0
Devin Leonhart <devinleonhart@fastmail.com>
2026-01-05 11:42:49 -0800

    Add filters.

--- spec/requests/untagged_images_spec.rb
+++ spec/requests/untagged_images_spec.rb
@@ -26,4 +26,19 @@ RSpec.describe "Untagged images filter", type: :request do
     expect(response.body).to include(edit_universe_image_path(universe, untagged_image))
     expect(response.body).not_to include(edit_universe_image_path(universe, tagged_image))
   end
+
+  it "preserves filter=untagged in pagination links" do
+    user = create(:user, password: "password123")
+    universe = create(:universe, owner: user)
+
+    # Force pagination (per_page is 20 on universes#show)
+    create_list(:image, 25, universe: universe)
+
+    sign_in_as(user)
+    get universe_path(universe, filter: "untagged")
+
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include("filter=untagged")
+    expect(response.body).to include("filter=untagged&amp;page=2")
+  end
 end
