5a83b822cbf0eb06ca17d18ad9a43a38e09f53b8
Devin Leonhart <devinleonhart@fastmail.com>
2026-01-07 22:21:33 -0800

    Remove bootstrap.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,6 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def flash_class(level)
-    levels = {
-      "notice" => "alert alert-info",
-      "success" => "alert alert-success",
-      "error" => "alert alert-danger",
-      "alert" => "alert alert-danger",
-    }
-    levels[level]
-  end
-
   def is_admin
     return current_user ? current_user.admin : false
   end
